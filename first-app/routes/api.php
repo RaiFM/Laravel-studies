0e4a7fdaf68a81e92f3cdbc8dec96ce92e7ae58d
RaiFM <sopararedesed@gmail.com>
2025-03-08 15:26:40 -0300

    comment user route

--- first-app/routes/api.php
+++ first-app/routes/api.php
@@ -19,4 +19,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 // Route::get('register', 'UserController@store')->name('user.store');
-Route::get('register', 'UserController@store');
+// Route::get('register', 'UserController@store');
