300122fe3113ca8e03f5b1ac753513ffbc9cef4f
RaiFM <sopararedesed@gmail.com>
2025-03-08 14:39:57 -0300

    explore routes

--- first-app/routes/web.php
+++ first-app/routes/web.php
@@ -2,21 +2,76 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/core', function(){
-    return view('empresa');
-});
+// call view directly
+    Route::view('/core', 'empresa');
+
+// Route naming and redirecting 
+    Route::get('/teste', function(){
+        return view('teste');
+    })->name('te');
+
+    Route::get('/deploy', function(){
+        return redirect()->route('te');
+    });
+    
+    Route::get('/deploy', function(){
+        return redirect()->route('adm.core');
+    });
+
+// Redirect route
+    Route::redirect('/sobre', '/empresa');
+
+// request types
+    Route::any('/any', function(){
+        return "request of any type";
+    });
+
+    Route::match(['get', 'post'], '/match', function(){
+        return "match request";
+    });
+
+    Route::get('/params/{t}/{p?}', function($p, $t = ''){
+        return "params request:" . $p . ' - ' . $t;
+    });
+
+// route groups
+
+    // all
+        Route::group([
+            'prefix' => 'adm',
+            'as' => 'adm.' 
+        ], function(){
+            Route::get('/teste', function(){
+                return "prefixed routes group: teste";
+            })->name('teste');
+            
+            Route::get('/core', function(){
+                return "prefixed routes group: core";
+            })->name('core');
+        });
+        
+    // prefix
+        Route::prefix('adm')->group(function(){
+            Route::get('/teste', function(){
+                return "prefixed routes group: teste";
+            });
+            
+            Route::get('/core', function(){
+                return "prefixed routes group: core";
+            });
+        });
+
+    // name
+        Route::name('adm.')->group(function(){
+            Route::get('adm/teste', function(){
+                return "named routes group: teste";
+            })->name('teste');
+            
+            Route::get('adm/core', function(){
+                return "named routes group: core";
+            })->name('core');
+        });
